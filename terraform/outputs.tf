5a4e5eaaacbe1d9949c89e23496099bc56a856c4
Brayan Ortega <brayansteven0322@gmail.com>
2025-04-08 19:19:29 -0500

    add terraform main.tf to create a vm in azure

---
+++ terraform/outputs.tf
@@ -0,0 +1,15 @@
+# Output the name of the resource group
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+# Output the public IP address of the virtual machine
+output "public_ip_address" {
+  value = azurerm_public_ip.public_ip.ip_address
+}
+
+# Output the admin password for the virtual machine
+output "admin_password" {
+  sensitive = true
+  value     = random_password.password.result
+}
