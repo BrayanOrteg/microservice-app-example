c4dc8b6344b092c09e840475cc3e477e17fb852e
AndresCamiloRR <101947895+AndresCamiloRR@users.noreply.github.com>
2025-04-15 17:34:53 -0500

    add zipkin - allow python logs in docker - debug logs in todos-api - change repo addres in go Dockerfile

--- todos-api/todoController.js
+++ todos-api/todoController.js
@@ -21,6 +21,7 @@ class TodoController {
     }
 
     create (req, res) {
+        console.log('Creating new todo item', req.body)
         // TODO: must be transactional and protected for concurrent access, but
         // the purpose of the whole example app it's enough
         const data = this._getTodoData(req.user.username)
@@ -53,13 +54,23 @@ class TodoController {
     _logOperation (opName, username, todoId) {
         this._tracer.scoped(() => {
             const traceId = this._tracer.id;
-            this._redisClient.publish(this._logChannel, JSON.stringify({
+            const message = JSON.stringify({
                 zipkinSpan: traceId,
                 opName: opName,
                 username: username,
                 todoId: todoId,
-            }))
-        })
+            });
+            
+            console.log(`Publishing to Redis channel ${this._logChannel}: ${message}`);
+            
+            this._redisClient.publish(this._logChannel, message, (err, reply) => {
+                if (err) {
+                    console.error('Error publishing to Redis:', err);
+                } else {
+                    console.log(`Successfully published to Redis, receivers: ${reply}`);
+                }
+            });
+        });
     }
 
     _getTodoData (userID) {
