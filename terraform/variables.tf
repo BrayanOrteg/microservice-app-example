fb1ad0d428ff2ec2ce7586c004fb40945c547e33
AndresCamiloRR <101947895+AndresCamiloRR@users.noreply.github.com>
2025-04-18 22:36:04 -0500

    vars error

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,9 +9,6 @@ variable "prefix" {
   description = "Prefix of the resource name"
 }
 
-variable "resource_group_location" { default     = "eastus" }
-variable "prefix"                { type = string; default = "win-vm-iis" }
-
 // Application settings
 
 variable "jwt_secret" {
