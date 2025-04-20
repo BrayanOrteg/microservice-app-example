6ee5cd6f8fff293a3595935786a396bbb6075fa4
AndresCamiloRR <101947895+AndresCamiloRR@users.noreply.github.com>
2025-04-20 16:34:20 -0500

    update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,6 +6,10 @@ output "resource_group_name" {
 # Output the public IP address of the virtual machine
 output "public_ip_address" {
   value = azurerm_public_ip.public_ip.ip_address
+  depends_on = [
+    azurerm_linux_virtual_machine.vm,
+    azurerm_network_interface.nic
+  ]
 }
 
 # Output the admin password for the virtual machine
